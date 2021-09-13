262cb14daffd075dba36759309bd43986fcd02ce
Hazzer <warlocksforge@gmail.com>
2021-09-13 19:26:40 +0200

    init commit

---
+++ Source/Main.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace HediffApplier
+{
+    [StaticConstructorOnStartup]
+    internal class Main
+    {
+        static Main()
+        {
+            try
+            {
+                var harmony = new Harmony("Harmony_FemaleHediff");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error during startup:\n{e}");
+            }
+
+        }
+    }
+}
---
+++ Source/XpHarmony_Patch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace HediffApplier
+{
+    [HarmonyPatch]
+    [HarmonyPatch(typeof(SkillRecord), "LearnRateFactor")]
+    class XpHarmony_Patch
+    {
+        static float Postfix(float __result, SkillRecord __instance, bool direct)
+        {
+            Pawn pawn = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
+            if (PawnChecker.MeetReq(pawn) && PawnChecker.HasHediff(pawn))
+            {
+                if (__instance.def.defName == "Melee" || __instance.def.defName == "Shooting")
+                {                    
+                    return __result * 0.7f;
+                }
+                if(__instance.def.defName == "Social")
+                {
+                    return __result * 1.5f;
+                }
+            }
+            return __result;
+        }
+    }
+}
